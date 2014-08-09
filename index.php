cb751a40f8747f3dd7e0b09832fc354c8f5b6b25
celloudiallo <mamadoucellou87@yahoo.fr>
2014-08-09 13:30:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Simple Landing Page</title>
+    <title>GuinéeBoulots</title>
     <meta name="description" content="Littlespark Coming soon page">
     <meta name="author" content="littlespark VT">
 
@@ -12,24 +12,24 @@
     <![endif]-->
 
     <!-- Le styles -->
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/custom.css" rel="stylesheet">
-    <link href="css/m-buttons.css" rel="stylesheet">
-    <link href="css/m-forms.css" rel="stylesheet">
-    <link href="css/countdown.css" rel="stylesheet">
-    <link href="css/validationEngine.jquery.css" rel="stylesheet">
+    <link href="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/css/bootstrap.css" rel="stylesheet">
+    <link href="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/css/custom.css" rel="stylesheet">
+    <link href="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/css/m-buttons.css" rel="stylesheet">
+    <link href="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/css/m-forms.css" rel="stylesheet">
+    <link href="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/css/countdown.css" rel="stylesheet">
+    <link href="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/css/validationEngine.jquery.css" rel="stylesheet">
 
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
 
     <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="images/favicon.ico">
-    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="shortcut icon" href="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/images/favicon.ico">
+    <link rel="apple-touch-icon" href="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/images/apple-touch-icon.png">
 
-    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/images/apple-touch-icon-114x114.png">
 
-    <script src="js/jquery.js"></script>
-    <script src="js/jquery.validationEngine.js" type="text/javascript"></script>
+    <script src="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/js/jquery.js"></script>
+    <script src="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/js/jquery.validationEngine.js" type="text/javascript"></script>
     <script type="text/javascript"> 
         $(document).ready(function() {
           // SUCCESS AJAX CALL, replace "success: false," by:     success : function() { callSuccessFunction() }, 
@@ -49,7 +49,7 @@
       <div class="row">
 
         <div class="span12">
-          <h1 class="logo">Simple Landing</h1>
+          <h1 class="logo">GuinéeBoulots</h1>
         </div>
 
 <!--         <div class="span12">
@@ -63,29 +63,29 @@
       </div>
       <div class="row">
         <div class="span6">
-          <h2 class="about">About This Template</h2>
-          <p class="info">This is a simple php based landing page that will help you collect information from your visitors via a validating php email form. We have included instructions to make it easy. Template built with <span class="heart"><3</span> by <a href="http://www.littlesparkvt.com">LittleSpark</a> If you have a minute, please check out our <a href="http://wrapbootstrap.com/user/littlesparkvt">other bootstrap templates </a>.</p>
+          <h2 class="about">Bienvenue</h2>
+          <p class="info">GuinéeBoulots est en reconstruction pour mieux vous servir. Nous serons bientôt de retour. Merci</p>
         </div>
 
 
 
         <div class="span6">
-          <img src="img/1.png">
+          <img src="http://test.guineeboulots.com/wp-content/uploads/2014/05/guineeboulots-logo-official.png">
           <div class="form">
 
    <div id="contact_form">
       <form id="formID" action="index.php" method="post">
         <p>
-          <input type="text" name="email" id="email" class="validate[required,custom[email]] text-input input-text m-wrap m-ctrl" placeholder="Your Email Address" />
-          <input type="submit" name="submit" id="submit" value="Sign Up" class="m-btn blue" />
+          <input type="text" name="email" id="email" class="validate[required,custom[email]] text-input input-text m-wrap m-ctrl" placeholder="Entrez votre email address" />
+          <input type="submit" name="submit" id="submit" value="Envoyer" class="m-btn blue" />
         </p>
       </form>
       <div id="code">
         <?php
         if (isset($_POST['submit']) or isset($_POST['submit_x']))
     {       
-          $to = "youremailaddress@email.com";
-          $subject = "Add this address to your mailing list";
+          $to = "info@guineeboulots.com";
+          $subject = "Information";
           $email_field = $_POST['email'];
           $body = "E-Mail\n$email_field";
            
@@ -104,7 +104,7 @@
     <div class="row">
       <div class="span12">
           
-          <h2 class="about">Time until site launch:</h2>
+          <h2 class="about">Temps jusqu'au lancement du site:</h2>
           <div id="counter">
             <!--counter is here -->
           </div>
@@ -116,7 +116,7 @@
       <footer>
         <div class="row">
         <div class="span4">
-          <p>&copy; <a href="http://www.yourcompany.com">Your Company</a> 2012</p>
+          <p>&copy; <a href="http://www.guineeboulots.com">GuinéeBoulots</a> 2014</p>
         </div>
         </div>
       </footer>
@@ -126,12 +126,12 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     
-  <script type="text/javascript" src="js/jquery.countdown.js"></script>
+  <script type="text/javascript" src="https://rawgit.com/celloudiallo/Simple-Landing-Page/master/js/jquery.countdown.js"></script>
     <script type="text/javascript">
 <!--
 // CHANGE THESE NUMBERS TO SPECIFY YOUR LAUNCH DATE
     $(function () {
-      var liftoffTime = new Date(2013, 6 - 1, 22, 11, 00);
+      var liftoffTime = new Date(2014, 8 - 1, 12, 11, 00);
       $('#counter').countdown({until: liftoffTime, 
         layout: '{dn} {dl}, {hn} {hl}, {mnn} {ml}, {snn} {sl}'});
     });
